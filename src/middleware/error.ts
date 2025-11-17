c7d0027767421da5ed954b7e632d95d686542c1f
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-11-16 21:56:03 -0300

    *Implemented Custom Error Handling Middleware & Error Classes     * error.ts Middleware:         - Centralized error handling for the application.         - Normalizes different error types into a consistent format.     * custom-error.ts         - Defines the abstract class for custom errors.         - Enforces implementation of statusCode and serializeErrors methods.         - Provides a base for all custom error classes.     * Specific Error Classes:         - ConflictError: Handles 409 Conflict errors.         - ForbiddenError: Handles 403 Forbidden errors.         - NotAuthorizedError: Handles 401 Unauthorized errors.         - NotFoundError: Handles 404 Not Found errors.         - RequestValidationError: Handles 400 Bad Request errors due to validation failures.         - DatabaseConnectionError: Handles database connection issues. 503 Service Unavailable.         - ServerError: Handles generic server errors. 500 Internal Server Error.     * Updated server.ts to integrate the error handling middleware.         - Ensures all errors are processed through the new middleware.         - If route does not exist, throws NotFoundError.
    *Updated dependencies:
        - express & @types/express to latest versions in package.json and package-lock.json.

---
+++ src/middleware/error.ts
@@ -0,0 +1,34 @@
+import type { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { CustomError } from "../errors/custom-error";
+// import { RequestValidationError } from "../errors/request-validation";
+// import { DatabaseConnectionError } from "../errors/database-connection";
+
+export const errorHandler: ErrorRequestHandler = (
+    error: Error, 
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+): void => {
+    //? General Error handling regardless of the Error Subclass
+    if (error instanceof CustomError) {
+        res.status(error.statusCode).json({ errors: error.serializeErrors() });
+        return;
+    }
+
+    /*
+        if (error instanceof RequestValidationError) {
+            res.status(error.statusCode).json({ errors: error.serializeErrors() });
+            return;
+        }
+
+        if(error instanceof DatabaseConnectionError) {
+            res.status(error.statusCode).json({ errors: error.serializeErrors() });
+            return;
+        } 
+    */
+
+    // if the error has now specific type, then just throw a generic error
+    res.status(500).json({ errors: [{
+        message: "Internal Server Error"
+    }] });
+};
