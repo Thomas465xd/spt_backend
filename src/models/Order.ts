e099e19f116bf0d72ce2e73b94474a14765f2f79
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-11-28 17:22:34 -0300

    *Order Model     * Attributes:         - userId: string         - items: Array<{             sku: string;             name: string;             price: number;             quantity: number;             lineTotal: number; // lineTotal = price * quantity         }>         - totalAmount: number         - orderStatus: 'Pendiente' | 'Entregado' | 'En Transito' | 'Cancelado'         - total: number         - payment: string         - shipper: string         - country: string         - createdAt: Date         - updatedAt: Date     * Model Methods:         - .build(): Static method to create a new Order instance     * Types:         - OrderAttrs: Interface defining the attributes required to create an Order         - OrderInterface: Interface extending mongoose.Document to include Order attributes         - OrderModel: Interface extending mongoose.Model to include the build method

---
+++ src/models/Order.ts
@@ -0,0 +1,152 @@
+import mongoose, { Schema, Document, Model, Types } from "mongoose";
+
+enum OrderStatus {
+    Pending = "Pendiente",
+    Sent = "En Transito", 
+    Delivered = "Entregado", 
+    Cancelled = "Cancelado"
+}; 
+
+interface OrderItem {
+    sku: string; 
+    name: string; 
+    price: number; 
+    quantity: number; 
+    lineTotal: number; // lineTotal = price * quantity
+}
+
+// Document interface = what exists after saving in Mongo
+// An interface that describes the properties that a Order Document has
+export interface OrderInterface extends Document {
+    //! Products
+    items: OrderItem[];
+    
+    payment: string
+    shipper: string
+    status: string
+    country: string
+
+    total: number
+
+    // Using populate will get other user data
+    // Either though businessRut & businessName is redundant here since we got the userId, 
+    // it can serve a searching purpose when trying to find all registered orders by
+    // a business, reducing the complexity of the query.
+    businessName: string
+    businessRut: string
+    userId: Types.ObjectId; 
+
+    //TODO: General Business Data
+
+    // ordering date
+    createdAt: Date; 
+    updatedAt: Date; 
+}
+
+// Attributes interface = what you must provide to create a order
+export interface OrderAttrs {
+    items: OrderItem[]
+    payment: string
+    shipper: string 
+    status?: OrderStatus
+    country: string 
+    total: number 
+
+    businessName: string 
+    businessRut: string
+    userId: Types.ObjectId
+}
+
+// Model interface = adds a build method that uses OrderAttrs
+// An interface that describes the properties that are required to create a new Order
+export interface OrderModel extends Model<OrderInterface> {
+    build(attrs: OrderAttrs): OrderInterface;
+}
+
+// Define the Order document Schema
+const orderSchema : Schema = new Schema(
+    {
+        items: [{
+            sku: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            price: {
+                type: Number,
+                required: true,
+                min: 0
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            lineTotal: {
+                type: Number,
+                required: true,
+                min: 0
+            }
+        }], 
+        payment: {
+            type: String, 
+            required: true, 
+            trim: true
+        },
+        shipper: {
+            type: String, 
+            required: true, 
+            trim: true
+        },
+        status: {
+            type: String,
+            enum: Object.values(OrderStatus),
+            default: OrderStatus.Pending,
+            required: true, 
+            trim: true
+        }, 
+        country: {
+            type: String, 
+            required: true, 
+            trim: true
+        }, 
+        total: {
+            type: Number, 
+            required: true, 
+            trim: true
+        },
+        businessName: {
+            type: String, 
+            required: true, 
+            trim: true
+        },
+        businessRut: {
+            type: String, 
+            required: true, 
+            trim: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId, 
+            required: true, 
+            ref: "User"
+        }, 
+    }, 
+    {
+        timestamps: true,
+    }
+);
+
+// Add  custom static "build" method
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order(attrs);
+}
+
+// Now when we call the Order constructor it already has typescript validation
+const Order = mongoose.model<OrderInterface, OrderModel>('Order', orderSchema)
+
+export default Order
