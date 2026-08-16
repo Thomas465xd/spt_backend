eb0d97b746e28fb2e19ccf5387b6c0be3bd4b962
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-08-16 17:54:41 -0400

    * Added environmental variables validation using env-var library     * env.ts:         - Added validations for all env variables present in .env.example file     * server.ts:         - Instead of validating env variables directly using if statements, now imports env.ts         and makes all validations at app startup and throws an error if any required env variable is missing or with an invalid type.

---
+++ src/config/env.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import * as env from "env-var";
+
+enum Environemnts {
+	Production = "production",
+	Development = "development",
+	CI = "ci",
+	Testing = "testing",
+}
+
+export const envs = {
+	NODE_ENV: env
+		.get("NODE_ENV")
+		.required()
+		.asEnum(Object.values(Environemnts)),
+	PORT: env.get("PORT").required().asPortNumber(),
+	DATABASE_URL: env.get("DATABASE_URL").required().asUrlString(),
+	FRONTEND_URL: env.get("FRONTEND_URL").required().asUrlString(),
+	ADMIN_EMAIL: env.get("ADMIN_EMAIL").required().asEmailString(),
+	JWT_SECRET: env.get("JWT_SECRET").required().asString(),
+	ADMIN_SECRET: env.get("ADMIN_SECRET").required().asString(),
+	RESEND_API_KEY: env.get("RESEND_API_KEY").required().asString(),
+	CONFIRMATION_SECRET: env.get("CONFIRMATION_SECRET").required().asString(),
+	PASSWORD_RESET_SECRET: env
+		.get("PASSWORD_RESET_SECRET")
+		.required()
+		.asString(),
+};
